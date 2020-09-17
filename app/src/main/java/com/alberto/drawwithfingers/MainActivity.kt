7ba7ef0e6711f919e8321d131214da41a4c9da44
Alberto ZT <clarck_beto_@hotmail.com>
2020-09-17 13:54:05 -0600

    Added java logic

--- app/src/main/java/com/alberto/drawwithfingers/MainActivity.kt
+++ app/src/main/java/com/alberto/drawwithfingers/MainActivity.kt
@@ -2,10 +2,18 @@ package com.alberto.drawwithfingers
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.DisplayMetrics
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        val displayMetrics = DisplayMetrics()
+        windowManager.defaultDisplay.getMetrics(displayMetrics)
+        paintView.init(displayMetrics)
+
     }
 }
